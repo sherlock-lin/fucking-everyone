79925497b05006238c0d9458bd01e30c3e606cef
carl-zero <1193179897@qq.com>
2023-02-18 14:07:28 +0800

    1. init project

---
+++ fucking-mybatis/src/main/java/com/example/fuckingmybatis/FuckingMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.example.fuckingmybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuckingMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FuckingMybatisApplication.class, args);
+    }
+
+}
---
+++ fucking-mybatis/src/test/java/com/example/fuckingmybatis/FuckingMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fuckingmybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuckingMybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
